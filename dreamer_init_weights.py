9b6a654a800c196b0a376c4cb36a684064d8fed8
antony-zhao <tonyzhao.davis@gmail.com>
2025-11-24 04:03:51 -0800

    working config 0

---
+++ dreamer_init_weights.py
@@ -0,0 +1,48 @@
+from torch import nn
+import numpy as np
+
+# Adapted from: https://github.com/NM512/dreamerv3-torch/blob/main/tools.py#L929
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        in_num = m.in_features
+        out_num = m.out_features
+        denoms = (in_num + out_num) / 2.0
+        scale = 1.0 / denoms
+        std = np.sqrt(scale) / 0.87962566103423978
+        nn.init.trunc_normal_(m.weight.data, mean=0.0, std=std, a=-2.0 * std, b=2.0 * std)
+        if hasattr(m.bias, "data"):
+            m.bias.data.fill_(0.0)
+    elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        space = m.kernel_size[0] * m.kernel_size[1]
+        in_num = space * m.in_channels
+        out_num = space * m.out_channels
+        denoms = (in_num + out_num) / 2.0
+        scale = 1.0 / denoms
+        std = np.sqrt(scale) / 0.87962566103423978
+        nn.init.trunc_normal_(m.weight.data, mean=0.0, std=std, a=-2.0, b=2.0)
+        if hasattr(m.bias, "data"):
+            m.bias.data.fill_(0.0)
+    elif isinstance(m, nn.LayerNorm):
+        m.weight.data.fill_(1.0)
+        if hasattr(m.bias, "data"):
+            m.bias.data.fill_(0.0)
+
+
+# Adapted from: https://github.com/NM512/dreamerv3-torch/blob/main/tools.py#L957
+def uniform_init_weights(given_scale):
+    def f(m):
+        if isinstance(m, nn.Linear):
+            in_num = m.in_features
+            out_num = m.out_features
+            denoms = (in_num + out_num) / 2.0
+            scale = given_scale / denoms
+            limit = np.sqrt(3 * scale)
+            nn.init.uniform_(m.weight.data, a=-limit, b=limit)
+            if hasattr(m.bias, "data"):
+                m.bias.data.fill_(0.0)
+        elif isinstance(m, nn.LayerNorm):
+            m.weight.data.fill_(1.0)
+            if hasattr(m.bias, "data"):
+                m.bias.data.fill_(0.0)
+
+    return f
--- model_surgery_atari.py
+++ model_surgery_atari.py
@@ -1,47 +1,38 @@
 import torch
+from dreamer_init_weights import *
 
-num_actions_new = None
-model_path = "ckpt_5000000_0.ckpt"
-new_path = "ckpt_start_assault.ckpt"
+def main(config):
+    model_path = config.model_path
+    new_path = config.new_path
 
-state = torch.load(model_path, map_location="cpu", weights_only=False)
-
-
-for k, v in state["actor"].items():
-    print(k, v.shape)
-for k, v in state["world_model"].items():
-    if "rssm" in k:
+    state = torch.load(model_path, map_location="cpu", weights_only=False)
+    for k, v in state["actor"].items():
         print(k, v.shape)
+    for k, v in state["world_model"].items():
+        if "rssm" in k:
+            print(k, v.shape)
+    if config.configuration == 0:
+        pass
+    elif config.configuration == 1: # replace actor head, TODO STILL
+        state["actor"]["mlp_heads.0.weight"].apply(uniform_init_weights(1.0))
+    elif config.configuration == 2: # replace entire actor
+        state["actor"].apply(init_weights)
+    elif config.configuration == 3: # replace encoder last and decoder first
+        pass
 
-old_act_dim = state["actor"]["mlp_heads.0.weight"].shape[0]
-new_act_dim = 6
+    state["iter_num"] = 0
+    state['last_log'] = 0
+    state['last_checkpoint'] = 0
 
-hidden = state["actor"]["mlp_heads.0.weight"].shape[1]
+    torch.save(state, new_path)
 
-new_actor_w = torch.nn.init.xavier_uniform_(torch.randn(new_act_dim, hidden)) #torch.randn(new_act_dim, hidden) * 0.01
-new_actor_b = torch.zeros(new_act_dim)
-
-copy_dim = min(old_act_dim, new_act_dim)
-new_actor_w[:copy_dim] = state["actor"]["mlp_heads.0.weight"][:copy_dim]
-new_actor_b[:copy_dim] = state["actor"]["mlp_heads.0.bias"][:copy_dim]
-
-state["actor"]["mlp_heads.0.weight"] = new_actor_w
-state["actor"]["mlp_heads.0.bias"] = new_actor_b
-
-old_in_dim = state["world_model"]["rssm.recurrent_model.mlp._model.0.weight"].shape[1]
-new_in_dim = old_in_dim + new_act_dim - old_act_dim
-
-old_w = state["world_model"]["rssm.recurrent_model.mlp._model.0.weight"]
-hidden = old_w.shape[0]
-
-new_w = torch.nn.init.xavier_uniform_(torch.randn(hidden, new_in_dim))
-
-copy_dim = old_in_dim - old_act_dim
-new_w[:, :copy_dim] = old_w[:, :copy_dim]
-
-state["world_model"]["rssm.recurrent_model.mlp._model.0.weight"] = new_w
-state["iter_num"] = 0
-state['last_log'] = 0
-state['last_checkpoint'] = 0
-
-torch.save(state, new_path)
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str)
+    parser.add_argument("--new_path", type=str)
+    parser.add_argument("--configuration", type=int, choices=[0, 1]) 
+    config = parser.parse_args()
+    main(config)
+    # 0 is basically no change outside of resetting some parameters
+    # 1 is ___
